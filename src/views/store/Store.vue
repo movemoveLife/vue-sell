6f9e867635f74078b5588d7acdc1f8e79ae8f6a5
柯颖昌 <ke.yingchang@yalla.live>
2024-09-12 22:38:16 +0800

    店铺title和背景设置成功

--- src/views/store/Store.vue
+++ src/views/store/Store.vue
@@ -1,3 +1,73 @@
 <template>
-    商店
+    <div class="storeDetails">
+        <Header title="店铺" />
+        <div class="content">
+            <div class="img"></div>
+            <div class="foodSort">
+                <div class="name">
+                    {{ data.title }}
+                    <img :src="data.img" alt="" class="store-img">
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
+
+<script>
+import Header from '@/components/Header.vue'
+export default {
+    components: {
+        Header
+    }
+}
+</script>
+<script setup>
+import { reactive, toRefs } from 'vue'
+let data = reactive({
+    title: '鱼拿酸菜鱼',
+    img: "https://img1.baidu.com/it/u=1599947592,1695977044&fm=253&fmt=auto&app=138&f=JPEG?w=640&h=440"
+})
+</script>
+
+<style lang="less" scoped>
+.storeDetails {
+    height: 100%;
+    display: flex;
+    flex-flow: column;
+
+    .content {
+        flex: 1;
+        overflow-y: auto;
+
+        .img {
+            background: url("../../assets/yuna.jpg") no-repeat center/cover;
+            width: 100%;
+            height: 150px;
+        }
+
+        .foodSort {
+            height: 500px;
+            background-color: #fff;
+            margin-top: -30px;
+            border-radius: 20px 20px 0 0;
+
+            .sort {
+                margin-top: 10px;
+            }
+
+            .name {
+                display: flex;
+                padding: 20px;
+                justify-content: space-between;
+
+                .store-img {
+                    width: 80px;
+                    height: 80px;
+                    border-radius: 10px;
+                    margin-top: -30px;
+                }
+            }
+        }
+    }
+}
+</style>
