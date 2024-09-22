5d3ba0e48771a3bbb9def8cc5e47966f9a77296d
柯颖昌 <ke.yingchang@yalla.live>
2024-09-22 11:22:52 +0800

    空页面组件和购物车列表页面的框架搭建

--- src/views/cart/Cart.vue
+++ src/views/cart/Cart.vue
@@ -1,18 +1,31 @@
 <template>
   <div class="cart">
-    <div class="content"></div>
+    <Header title="购物车" />
+    <CartDetail v-if="isShow" />
+    <Empty v-else />
     <Footer />
   </div>
 </template>
 
-<script>
-import Footer from '@/components/Footer.vue';
-export default {
-  name: 'Cart',
-  components: {
-    Footer
-  },
-}  
+<script setup>
+import Header from '@/components/Header.vue'
+import Footer from '@/components/Footer.vue'
+import Empty from '@/components/Empty.vue'
+import CartDetail from './components/CartDetail.vue'
+import { ref, onMounted } from 'vue'
+import { useStore } from 'vuex'
+
+const isShow = ref(true)
+const store = useStore()
+
+const init = () => {
+  if (store.state.cartList.length === 0) {
+    isShow.value = false
+  }
+}
+onMounted(() => {
+  init()
+})
 </script>
 
 <style lang="less" scoped>
---
+++ src/views/cart/components/CartDetail.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="cart-detail">
+        购物车列表
+    </div>
+</template>
